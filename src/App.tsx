dc312cbe9a717fdf297c419adc1655444e571e7d
zana-abdollahpour <zana.ap4@gmail.com>
2025-02-04 19:58:21 +0330

    example: image gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
+import Image from "@/components/Image";
+
+import { cn } from "@/utils";
+
 export default function App() {
-  return <h1>initial commit</h1>;
+  return (
+    <section className="inline-grid w-full bg-amber-100 text-white">
+      <div
+        className={cn(
+          "inline-grid grid-cols-3 justify-center justify-items-center",
+          "m-2 mx-auto max-w-screen-2xl gap-4",
+        )}
+      >
+        <p className="col-span-full p-2 text-4xl font-black text-amber-950">
+          Giggity
+        </p>
+        {Array.from({ length: 12 })
+          .fill(0)
+          .map((_, index) => (
+            <div
+              key={index}
+              className={cn(
+                `w-fit overflow-hidden rounded-lg bg-amber-200`,
+                index % 2 === 0 && "self-center",
+                index % 4 === 0 && "col-span-3",
+              )}
+            >
+              <Image photo={index % 2 === 0 ? "brunette" : "ginger"} />
+            </div>
+          ))}
+        <p className="col-span-full p-2 text-4xl font-black text-amber-950">
+          Giggity
+        </p>
+      </div>
+    </section>
+  );
 }
